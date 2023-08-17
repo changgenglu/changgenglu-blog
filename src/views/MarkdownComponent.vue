971433ea9b0e9923e906f53b64f00b42ed2de2ca
Ivan_Ma <ivan@rexlite.com.tw>
2023-08-17 15:36:47 +0800

    feat: 移除多於程式碼

--- src/views/MarkdownComponent.vue
+++ src/views/MarkdownComponent.vue
@@ -61,25 +61,10 @@ export default {
                 this.isMobile = false;
             }
         },
-        addLang() {
-            let getCodeType = document.getElementsByTagName('code');
-            let attrData = document.getElementsByClassName('code-wrapper');
-            let codeType = [];
-
-            for (let i = 0; i < getCodeType.length; i++) {
-                codeType = getCodeType[i].className.split(" ")[1];
-                attrData[i].setAttribute("data-rel", codeType);
-
-                codeType === 'applescript' ?
-                    attrData[i].setAttribute("data-rel", '') :
-                    attrData[i].setAttribute("data-rel", codeType);
-            }
-        }
     },
     mounted() {
         this.loadMarkdown();
         this.checkDevice();
-        this.addLang();
     },
 }
 </script>
